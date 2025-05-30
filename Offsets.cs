13e08d61c7e27a285b101c0e1a531324a23827af
Mohammed Farhan <mohammedfarhan1441@gmail.com>
2025-05-30 10:39:10 +0530

    Initial commit

---
+++ Offsets.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace AotForms
+{
+    internal static class Offsets
+    {
+
+
+        internal static uint GameTimer = 0x10; // 0x10
+        internal static uint FixedDeltaTime = 0x24; // 0x24
+
+        internal static uint Il2Cpp;
+        internal static uint InitBase = 0x8CA88B4;
+        internal static uint StaticClass = 0x5c;
+
+        internal static uint CurrentMatch = 0x50;
+        internal static uint MatchStatus = 0x8;
+        internal static uint LocalPlayer = 0x44;
+        internal static uint DictionaryEntities = 0x68;
+        internal static uint Player_IsDead = 0x4C;
+        internal static uint Player_Name = 0x224;
+        internal static uint Player_Data = 0x44;
+        internal static uint Player_ShadowBase = 0x11d0;
+        internal static uint XPose = 0x78;
+
+        internal static uint AvatarManager = 0x3f4;
+        internal static uint Avatar = 0x94;
+        internal static uint Avatar_IsVisible = 0x7c;
+        internal static uint Avatar_Data = 0x10;
+        internal static uint Avatar_Data_IsTeam = 0x51;
+
+        internal static uint FollowCamera = 0x384;
+        internal static uint Camera = 0x14;
+        internal static uint AimRotation = 0x33C;
+        internal static uint MainCameraTransform = 0x194;
+
+        internal static uint Weapon = 0x330;
+        internal static uint WeaponData = 0x44;
+        internal static uint WeaponRecoil = 0xc;
+
+        internal static uint ViewMatrix = 0x98 + 0x24;
+
+        internal static uint IS_FIRING = 0x474; //Class_COW_GameFacade  //private Boolean <LPEIEILIKGC>k__BackingField; // 0x454
+        internal static uint weaponinfo = 0x808; // internal class Player : AttackableEntity -> private MADMMIICBNN GEGFCFDGGGP;
+        internal static uint guntipposition = 0x38; //private Vector3 BIIBCBNNDHB; // 0x38 //public Vector3 BOGOIAMJFDN
+        internal static uint bullet_hit = 0x2C; //public Vector3 NHKKHPLFMNG; // 0x2c
+
+        internal static uint CurrentObserver = 0x64;// internal class NFJPHMKKEBF -> protected FNCMBMMKLLI BGGJJKKKFDC;
+        internal static uint ObserverPlayer = 0x28;// internal class FNCMBMMKLLI -> private Player NJMDHHGDNPJ;
+
+
+
+    }
+}
