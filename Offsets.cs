b21dbda5205060ae765a80168d57eda282c77b7a
mohammedfarhan14 <62455136+mohammedfarhan14@users.noreply.github.com>
2025-05-30 10:40:09 +0530

    Update Offsets.cs

--- Offsets.cs
+++ Offsets.cs
@@ -40,13 +40,13 @@ namespace AotForms
 
         internal static uint ViewMatrix = 0x98 + 0x24;
 
-        internal static uint IS_FIRING = 0x474; //Class_COW_GameFacade  //private Boolean <LPEIEILIKGC>k__BackingField; // 0x454
-        internal static uint weaponinfo = 0x808; // internal class Player : AttackableEntity -> private MADMMIICBNN GEGFCFDGGGP;
-        internal static uint guntipposition = 0x38; //private Vector3 BIIBCBNNDHB; // 0x38 //public Vector3 BOGOIAMJFDN
-        internal static uint bullet_hit = 0x2C; //public Vector3 NHKKHPLFMNG; // 0x2c
+        internal static uint IS_FIRING = 0x474; 
+        internal static uint weaponinfo = 0x808;
+        internal static uint guntipposition = 0x38; 
+        internal static uint bullet_hit = 0x2C; 
 
-        internal static uint CurrentObserver = 0x64;// internal class NFJPHMKKEBF -> protected FNCMBMMKLLI BGGJJKKKFDC;
-        internal static uint ObserverPlayer = 0x28;// internal class FNCMBMMKLLI -> private Player NJMDHHGDNPJ;
+        internal static uint CurrentObserver = 0x64;
+        internal static uint ObserverPlayer = 0x28;
 
 
 
